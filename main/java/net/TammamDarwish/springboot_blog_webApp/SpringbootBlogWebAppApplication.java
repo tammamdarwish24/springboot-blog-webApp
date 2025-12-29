25713e011a58c47dd626875f0dec8cad8e4dcea9
tammamdarwish24 <tammamdarwish24@gmail.com>
2025-12-29 20:38:37 +0300

    second commit2

---
+++ main/java/net/TammamDarwish/springboot_blog_webApp/SpringbootBlogWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.TammamDarwish.springboot_blog_webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogWebAppApplication.class, args);
+	}
+
+}
---
+++ test/java/net/TammamDarwish/springboot_blog_webApp/SpringbootBlogWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.TammamDarwish.springboot_blog_webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
